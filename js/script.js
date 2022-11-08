87602e7728f10d34df11c220904249a4088df0cd
RominaMalimban <romina.lyn@hotmail.it>
2022-11-08 15:46:43 +0100

    evento click e creazione variabili nome  e km

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // Riscrivere il programma del calcolo del biglietto del treno in versione completa:
 
+
+
+// salvo in una variabile il riferimento al bottone "genera":
+let myButton = document.getElementById("btn");
+
+// creo l'evento:
+myButton.addEventListener("click",
+
+    function(){
+        // salvo in una variabile il riferimento a nome e cognome del pax:
+        let inputNome = document.getElementById("nome");
+        console.log(inputNome);
+        let nomePax = inputNome.value;
+        console.log(nomePax);
+
+        // salvo in una variabile il riferimento al numero di chilometri da percorrere:
+        let inputKm = document.getElementById("chilometri");
+        console.log(inputKm);
+        let numeroKm = inputKm.value;
+        console.log(numeroKm);
+    }
+);
